ad80f0d34cfe37a928ec6154acd768f20f0321d4
vinhng2301 <ducvinhnguyen0201@gmail.com>
2022-07-13 17:10:14 +0700

    done test 30/4/2018

--- src/NextDayCalculator.java
+++ src/NextDayCalculator.java
@@ -3,10 +3,32 @@ public class NextDayCalculator {
     public static final String Gachcheo = "/";
 
     public static String getNextDay(int day, int month, int year) {
-        if (day == 31) {
-            day = 0;
-            month += 1;
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+                if(day == 31) {
+                    return 1 + Gachcheo + (month+1) + Gachcheo + year;
+                }
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                if (day == 30) {
+                    return 1 + Gachcheo + (month+1) + Gachcheo + year;
+                }
+                break;
+            case 2:
+                if (day == 28) {
+                    return 1 + Gachcheo + (month+1) + Gachcheo + year;
+                }
+            default:
+                return 1 + Gachcheo + (month+1) + Gachcheo + year;
         }
-        return day+1 + Gachcheo + month + Gachcheo + year;
+        return (day + 1) + Gachcheo + month + Gachcheo + year;
     }
 }
